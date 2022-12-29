bb60d10b2b75c25e153a2249596c0ef27d6337cf
Gaëtan H <steellgold0@gmail.com>
2022-12-29 20:03:23 +0100

    🛰️ Update user and delete user | some new routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -16,29 +16,36 @@ type Body = {
 }
 
 /** GET ALL USERS */
-app.route({ method: "GET", url: "/users/:secretKey", handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
-  checkEnvKey(request.params.secretKey, reply);
+app.route({ method: "GET", url: "/users/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
 
-  const users = await prisma.users.findMany();
-  reply.send(patch(users));
-} } as RouteOptions);
+    const users = await prisma.users.findMany();
+    reply.send(patch(users));
+  }
+} as RouteOptions);
 
 /** GET A USER BY USER_IDENTIFIER */
 app.route({ method: "GET", url: "/users/:userIdentifier/:secretKey",
   handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
     checkEnvKey(request.params.secretKey, reply);
-    const user = await prisma.users.findMany({
-      where: {
-        userIdentifier: String(request.params.userIdentifier)
-      }
-    });
 
-    if (user == null) {
-      reply.code(404).send({ message: "User not found" });
-      return;
-    }
-
+    const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+    if (!user[0]) return reply.code(404).send({ message: "User not found" });
     reply.send(patch(user[0]));
+  }
+} as RouteOptions);
+
+/** GET TASKS OF A USER */
+app.route({ method: "GET", url: "/users/:userIdentifier/tasks/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
+
+    const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+    if (!user[0]) return reply.code(404).send({ message: "User not found" });
+
+    const tasks = await prisma.tasks.findMany({ where: { taskAuthor: String(request.params.userIdentifier) } });
+    reply.send(patch(tasks));
   }
 } as RouteOptions);
 
@@ -48,11 +55,7 @@ app.route({ method: "GET", url: "/users/:userIdentifier/tasks/count/:secretKey",
     checkEnvKey(request.params.secretKey, reply);
 
     const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
-
-    if (user == null) {
-      reply.code(404).send({ message: "User not found" });
-      return;
-    }
+    if (!user[0]) return reply.code(404).send({ message: "User not found" });
 
     const tasks = await prisma.tasks.findMany({ where: { taskAuthor: String(request.params.userIdentifier) } });
     reply.send(patch(tasks.length));
@@ -65,5 +68,32 @@ app.route({ method: "POST", url: "/users/create/:secretKey",
     checkEnvKey(request.params.secretKey, reply);
 
     reply.send(patch(await prisma.users.create({ data: request.body })));
+  }
+} as RouteOptions);
+
+/** UPDATE A USER */
+app.route({ method: "PUT", url: "/users/:userIdentifier/update/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params, Body: Body }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
+
+    const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+    if (!user[0]) return reply.code(404).send({ message: "User not found" });
+
+    reply.send(patch(await prisma.users.update({
+      where: { id: user[0].id },
+      data: request.body
+    })));
+  }
+} as RouteOptions);
+
+/** DELETE A USER */
+app.route({ method: "DELETE", url: "/users/:userIdentifier/delete/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
+
+    const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+    if (!user[0]) return reply.code(404).send({ message: "User not found" });
+
+    reply.send(patch(await prisma.users.delete({ where: { id: user[0].id } })));
   }
 } as RouteOptions);
