3beb4dc1dfccaa3d56530de634d95ebcc82acf37
Gaëtan H <steellgold0@gmail.com>
2022-12-29 01:12:07 +0100

    🛰️ Fastify app (default)

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { fastifyAutoload as autoload } from "@fastify/autoload"
+import path from 'path';
+
+const prisma = new PrismaClient();
+const app = fastify();
+
+app.get('/', async (request, reply) => {
+  const allUsers = await prisma.users.findMany();
+  
+  return JSON.parse(JSON.stringify(allUsers, (_, value) => typeof value === "bigint"
+    ? value.toString()
+    : value
+  ));
+});
+
+app.listen({
+  port: 3000
+}).then(() => console.log('Server is running on http://localhost:3000'));
