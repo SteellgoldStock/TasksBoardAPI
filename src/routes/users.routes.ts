c8959ba84fb7ec2aadd143439da1bc3a84b75acd
Gaëtan H <steellgold0@gmail.com>
2022-12-29 18:34:50 +0100

    🧔 Edit some variables and routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,33 +1,32 @@
+import { FastifyReply, FastifyRequest } from "fastify";
 import { RouteOptions } from "fastify/types/route";
 import { app } from "..";
 import prisma, { patch } from "../utils/prisma";
+import { checkEnvKey } from "../utils/Validator";
+
+type Params = {
+  userIdentifier: string;
+  secretKey: string;
+}
+
+type Body = {
+  userIdentifier: string;
+  userSecret: string;
+  userName: string;
+}
 
 /** GET ALL USERS */
-app.route({
-  method: "GET",
-  url: "/users/:secretKey",
-  handler: async(request: any, reply: any) => {
-    if (request.params.secretKey !== process.env.SECRET_TOKEN) {
-      reply.code(401).send({ message: "Unauthorized" });
-      return;
-    }
+app.route({ method: "GET", url: "/users/:secretKey", handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+  checkEnvKey(request.params.secretKey, reply);
 
-    const users = await prisma.users.findMany();
-    reply.send(patch(users));
-  }
-} as RouteOptions);
+  const users = await prisma.users.findMany();
+  reply.send(patch(users));
+} } as RouteOptions);
 
 /** GET A USER BY USER_IDENTIFIER */
-app.route({
-  method: "GET",
-  url: "/users/:userIdentifier/:secretKey",
-  handler: async(request: any, reply: any) => {
-
-    if (request.params.secretKey !== process.env.SECRET_TOKEN) {
-      reply.code(401).send({ message: "Unauthorized" });
-      return;
-    }
-
+app.route({ method: "GET", url: "/users/:userIdentifier/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
     const user = await prisma.users.findMany({
       where: {
         userIdentifier: String(request.params.userIdentifier)
@@ -43,17 +42,28 @@ app.route({
   }
 } as RouteOptions);
 
-/** CREATE A NEW USER */
-app.route({
-  method: "POST",
-  url: "/users/create/:secretKey",
-  handler: async(request: any, reply: any) => {
-    if (request.params.secretKey !== process.env.SECRET_TOKEN) {
-      reply.code(401).send({ message: "Unauthorized" });
+/** GET THE COUNT OF TASKS OF A USER */
+app.route({ method: "GET", url: "/users/:userIdentifier/tasks/count/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
+
+    const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+
+    if (user == null) {
+      reply.code(404).send({ message: "User not found" });
       return;
     }
 
-    const user = await prisma.users.create({ data: request.body });
-    reply.send(patch(user));
+    const tasks = await prisma.tasks.findMany({ where: { taskAuthor: String(request.params.userIdentifier) } });
+    reply.send(patch(tasks.length));
+  }
+} as RouteOptions);
+
+/** CREATE A NEW USER */
+app.route({ method: "POST", url: "/users/create/:secretKey",
+  handler: async(request: FastifyRequest<{ Params: Params, Body: Body }>, reply: FastifyReply) => {
+    checkEnvKey(request.params.secretKey, reply);
+
+    reply.send(patch(await prisma.users.create({ data: request.body })));
   }
 } as RouteOptions);
