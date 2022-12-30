f5832cbe997f264029be4fce8be45300232000c7
Gaëtan H <steellgold0@gmail.com>
2022-12-30 17:58:25 +0100

    🛡️ Query body schema with Zod

--- src/routes/tasks/post.ts
+++ src/routes/tasks/post.ts
@@ -2,9 +2,21 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { app } from "../..";
 import { Types } from "../../types/Types";
 import prisma, { patch } from "../../utils/prisma";
+import z from "zod";
 
 app.post("/tasks/create/:userIdentifier", {
   handler: async(request: FastifyRequest<{ Params: Types["Params"], Body: Types["Body"] }>, reply: FastifyReply) => {
+    const bodySchema = z.object({
+      taskIdentifier: z.string().min(32).max(32),
+      taskAuthor: z.string().min(32).max(32),
+      taskTitle: z.string().min(5).max(25),
+      taskContent: z.string().min(5).max(10000),
+      isCompleted: z.boolean().default(false)
+    });
+
+    const bodyParsed = bodySchema.safeParse(request.body);
+    if (!bodyParsed.success) return reply.code(400).send({ message: "Invalid Body" });
+
     reply.send(patch(await prisma.tasks.create({ data: request.body })));
   }
 });
--- src/routes/users/post.ts
+++ src/routes/users/post.ts
@@ -2,9 +2,19 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { app } from "../..";
 import { Types } from "../../types/Types";
 import prisma, { patch } from "../../utils/prisma";
+import z from "zod";
 
 app.post("/users/create", {
   handler: async(request: FastifyRequest<{ Params: Types["Params"], Body: Types["Body"] }>, reply: FastifyReply) => {
+    const schema = z.object({
+      userIdentifier: z.string().min(32).max(32),
+      userSecret: z.string().min(25).max(25),
+      userName: z.string().min(5).max(25)
+    });
+
+    const parsed = schema.safeParse(request.body);
+    if (!parsed.success) return reply.code(400).send({ message: "Invalid Body" });
+
     reply.send(patch(await prisma.users.create({ data: request.body })));
   }
 });
