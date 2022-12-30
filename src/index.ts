0e6f7c6fc915319a66b1472eb5113c135cb9445d
Gaëtan H <steellgold0@gmail.com>
2022-12-30 14:52:06 +0100

    🔒 Authorization Bearer (Tasks)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { Types } from "./types/Types";
+import prisma from "./utils/prisma";
 export const app = fastify();
 
 async function server() {
@@ -9,11 +11,19 @@ async function server() {
 
   import("./routes/tasks/get");
 
-  app.addHook("preHandler", async(request: FastifyRequest, reply: FastifyReply) => {
+  app.addHook("preHandler", async(request: FastifyRequest<{ Params: Types["Params"] }>, reply: FastifyReply) => {
+    const secretToken = request.headers.authorization?.split(" ")[1];
+
     if (request.url.startsWith("/users")) {
-      if (request.headers.authorization?.split(" ")[1] !== process.env.SECRET_TOKEN) {
-        reply.code(401).send({ message: "Unauthorized" });
-        return;
+      if (secretToken !== process.env.SECRET_TOKEN) {
+        return reply.code(401).send({ message: "Unauthorized" });
+      }
+    } else if (request.url.startsWith("/tasks")) {
+      const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
+      if (!user[0]) return reply.code(404).send({ message: "User not found" });
+
+      if (user[0].userSecret !== secretToken) {
+        return reply.code(401).send({ message: "Unauthorized" });
       }
     }
   });
