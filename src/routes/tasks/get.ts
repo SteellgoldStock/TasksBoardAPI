33e1ea9700c0a13ad5ac753534b01fbf9f292d95
Gaëtan H <steellgold0@gmail.com>
2022-12-30 14:44:55 +0100

    🗑️ Not really useful

--- src/routes/tasks/get.ts
+++ src/routes/tasks/get.ts
@@ -3,13 +3,6 @@ import { app } from "../..";
 import { Types } from "../../types/Types";
 import prisma, { patch } from "../../utils/prisma";
 
-app.get("/tasks/all", {
-  handler: async(request: FastifyRequest<{ Params: Types["Params"] }>, reply: FastifyReply) => {
-    const tasks = await prisma.tasks.findMany();
-    reply.send(patch(tasks));
-  }
-});
-
 app.get("/tasks/:userIdentifier", {
   handler: async(request: FastifyRequest<{ Params: Types["Params"] }>, reply: FastifyReply) => {
     const tasks = await prisma.tasks.findMany({
