d8e1e80d85d675e3ca6a83a841b8b1e38aa8d0a9
Gaëtan H <steellgold0@gmail.com>
2023-01-02 16:27:23 +0100

    🛡️ Implementation of JWT

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { Types } from "./types/Types";
 import prisma from "./utils/prisma";
 import jwt from "jsonwebtoken";
+import { z } from "zod";
 export const app = fastify();
 
 async function server() {
@@ -16,13 +17,31 @@ async function server() {
 
   app.addHook("preHandler", async(request: FastifyRequest<{ Params: Types["TaskParams"] }>, reply: FastifyReply) => {
     const secretToken = request.headers.authorization?.split(" ")[1];
+    if (!secretToken) return reply.code(401).send({ message: "Unauthorized: Secret missing" });
 
     if (request.url.startsWith("/users")) {
+      // For UPDATE, DELETE and GET need JWT
+      // For POST need SECRET_TOKEN
       if (secretToken !== process.env.SECRET_TOKEN) return reply.code(401).send({ message: "Unauthorized" });
     } else if (request.url.startsWith("/tasks")) {
-      const user = await prisma.users.findMany({ where: { userIdentifier: String(request.params.userIdentifier) } });
-      if (!user[0]) return reply.code(404).send({ message: "User not found" });
-      if (user[0].userSecret !== secretToken) return reply.code(401).send({ message: "Unauthorized" });
+      try {
+        const decoded = jwt.decode(secretToken);
+
+        const decodedSchema = z.object({
+          user: z.string().min(36).max(36),
+          secret: z.string().min(25).max(25)
+        }).safeParse(decoded);
+
+        if (!decodedSchema.success) return reply.code(400).send({ message: "Bad Request: Invalid authorization bearer token" });
+        const user = await prisma.users.findMany({ where: {
+          userIdentifier: String(decodedSchema.data.user),
+          userSecret: String(decodedSchema.data.secret)
+        } });
+
+        if (!user[0]) return reply.code(404).send({ message: "User not found" });
+      } catch (err) {
+        return reply.code(400).send({ message: "Bad Request: Invalid authorization bearer token" });
+      }
     }
   });
 
