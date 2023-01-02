37f3b1413dbc7e244b56b2d568f837794338cdb1
Gaëtan H <steellgold0@gmail.com>
2023-01-02 16:32:34 +0100

    🛡️ Check if user already exist

--- src/routes/users/post.ts
+++ src/routes/users/post.ts
@@ -4,6 +4,7 @@ import { Types } from "../../types/Types";
 import prisma, { patch } from "../../utils/prisma";
 import z from "zod";
 
+// TODO: Email, Password (For Auth: todo)
 app.post("/users/create", {
   handler: async(request: FastifyRequest<{ Params: Types["UserParams"], Body: Types["UserBody"] }>, reply: FastifyReply) => {
     const bodySchema = z.object({
@@ -11,6 +12,9 @@ app.post("/users/create", {
       userSecret: z.string().min(25).max(25),
       userName: z.string().min(5).max(25)
     }).safeParse(request.body);
+
+    const userIdentifier = await prisma.users.findMany({ where: { userIdentifier: request.body.userIdentifier } });
+    if (userIdentifier[0]) return reply.code(400).send({ message: "User Identifier already use" });
 
     if (!bodySchema.success) return reply.code(400).send({ message: "Invalid Body" });
     reply.send(patch(await prisma.users.create({ data: request.body })));
